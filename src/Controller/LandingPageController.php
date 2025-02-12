00db0082342d5e10e86f3a605a58641178963d7e
Mohamed Ali Saadaoui <mohamedali.saadaoui@esprit.tn>
2025-02-12 13:45:54 +0100

    Intégration du template front avec le template back-office

---
+++ src/Controller/LandingPageController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LandingPageController extends AbstractController
+{
+    #[Route('/Landing', name: 'admin_dashboard')]
+    public function index(): Response
+    {
+        return $this->render('LandingPage/LandingPage.html.twig');
+    }
+}
+
