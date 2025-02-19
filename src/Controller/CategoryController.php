07f6a78816b64deca1093753c6166b03a4327846
Mohamed Ali Saadaoui <mohamedali.saadaoui@esprit.tn>
2025-02-20 00:38:29 +0100

    Afficher les produits dans la page liste des produits.

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,17 +2,25 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class CategoryController extends AbstractController
 {
     #[Route('/category', name: 'app_category')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        // Fetch all products from the database
+        $products = $entityManager->getRepository(Product::class)->findAll();
+
+        // Render the template and pass the products to it
         return $this->render('category/category.html.twig', [
             'controller_name' => 'CategoryController',
+            'products' => $products,  // Passing products to the template
         ]);
     }
 }
+
