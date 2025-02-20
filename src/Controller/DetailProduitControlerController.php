4974f43fed102cb10366ac5406ecc373620d2ffd
Mohamed Ali Saadaoui <mohamedali.saadaoui@esprit.tn>
2025-02-20 02:52:05 +0100

    Ajouter le produit dans le panier et les CRUD sur le produit (supprimer le produit, voir les détails du produit).

--- src/Controller/DetailProduitControlerController.php
+++ src/Controller/DetailProduitControlerController.php
@@ -1,18 +1,24 @@
 <?php
-
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ProductRepository;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DetailProduitControlerController extends AbstractController
 {
-    #[Route('/detailproduitcontroler', name: 'app_detail_produit_controler')]
-    public function index(): Response
-    {
-        return $this->render('detail_produit_controler/detailproduit.html.twig', [
-            'controller_name' => 'DetailProduitControlerController',
-        ]);
+    #[Route('/produit/{id}', name: 'app_detail_produit_controler')]
+public function detailProduit($id, ProductRepository $productRepository): Response
+{
+    $product = $productRepository->find($id);
+
+    if (!$product) {
+        throw $this->createNotFoundException('Produit non trouvé');
     }
+
+    return $this->render('detail_produit_controler/detailproduit.html.twig', [
+        'product' => $product,
+    ]);
+}
 }
--- src/Controller/PanierControlerController.php
+++ src/Controller/PanierControlerController.php
@@ -3,16 +3,45 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use App\Repository\ProductRepository;
+
 
 class PanierControlerController extends AbstractController
 {
-    #[Route('/panier', name: 'app_panier_controler')]
-    public function index(): Response
-    {
-        return $this->render('panier_controler/panier.html.twig', [
-            'controller_name' => 'PanierControlerController',
-        ]);
+    #[Route('/panier/add/{id}', name: 'app_panier_add')]
+public function addToCart($id, ProductRepository $productRepository, SessionInterface $session): Response
+{
+    // Retrieve the product from the database
+    $product = $productRepository->find($id);
+
+    if (!$product) {
+        throw $this->createNotFoundException('Produit non trouvé');
     }
+
+    // Retrieve the cart from the session
+    $panier = $session->get('panier', []);
+
+    // Add or update the product in the cart
+    if (isset($panier[$id])) {
+        $panier[$id]['quantity'] += 1; // Increment quantity if the product is already in the cart
+    } else {
+        // Add new product to the cart with its details
+        $panier[$id] = [
+            'id' => $product->getId(),
+            'name' => $product->getName(),
+            'quantity' => 1,
+        ];
+    }
+
+    // Save the updated cart back into the session
+    $session->set('panier', $panier);
+
+    // Redirect back to the cart page
+    return $this->redirectToRoute('app_panier_controler');
 }
+}
+
